20a28ab128bff59def40f52a88e9351a9a548b3a
yongckim <zxcv9203@naver.com>
2026-07-02 21:55:59 +0900

    :art: style : .editorconfig 규칙에 맞춰 기존 파일 리포맷

--- src/test/java/com/ssoss/ssossbackend/SsossBackendApplicationTests.java
+++ src/test/java/com/ssoss/ssossbackend/SsossBackendApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class SsossBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
