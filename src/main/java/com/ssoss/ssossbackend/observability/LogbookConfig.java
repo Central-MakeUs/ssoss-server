5230ab3589872acbaff8f74c4dd459f611f874e2
yongckim <zxcv9203@naver.com>
2026-07-12 18:20:14 +0900

    :lock: security : Logbook HTTP 로그의 토큰 필드 마스킹

--- src/main/java/com/ssoss/ssossbackend/observability/LogbookConfig.java
+++ src/main/java/com/ssoss/ssossbackend/observability/LogbookConfig.java
@@ -5,18 +5,25 @@ import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.BodyFilter;
 import org.zalando.logbook.HeaderFilter;
 import org.zalando.logbook.core.HeaderFilters;
+import org.zalando.logbook.json.JsonBodyFilters;
 
 @Configuration
-public class LogbookConfig {
+class LogbookConfig {
 
     private static final Set<String> LOGGED_HEADERS = Set.of("content-type", "authorization");
+    private static final Set<String> MASKED_BODY_PROPERTIES = Set.of("accessToken", "refreshToken");
 
     @Bean
     HeaderFilter loggedHeaderFilter() {
         HeaderFilter allowlist = HeaderFilters.removeHeaders(name -> !LOGGED_HEADERS.contains(name.toLowerCase(Locale.ROOT)));
         return HeaderFilter.merge(allowlist, HeaderFilters.authorization());
     }
-}
 
+    @Bean
+    BodyFilter credentialMaskingBodyFilter() {
+        return JsonBodyFilters.replaceJsonStringProperty(MASKED_BODY_PROPERTIES, "***");
+    }
+}
