71ab07f64eaa3e39c4f91dbdb46120a98a9fdc93
yongckim <zxcv9203@naver.com>
2026-07-23 00:24:17 +0900

    :wrench: chore : 비동기 가상 스레드 실행 활성화

---
+++ src/main/java/com/ssoss/ssossbackend/async/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.ssoss.ssossbackend.async;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+}
